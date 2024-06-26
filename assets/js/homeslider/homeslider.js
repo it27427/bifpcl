d165c6266854104c05dc3956d284082a13a4e7bc
it27427 <istiak.tushar.qtec@gmail.com>
2024-06-26 14:18:40 +0600

    boardofdirectors completed

---
+++ assets/js/homeslider/homeslider.js
@@ -0,0 +1,24 @@
+const directorSlider = new Swiper('.directorSlider', {
+  slidesPerView: 1,
+  spaceBetween: 24,
+  centeredSlides: false,
+  autoplay: {
+    delay: 3000,
+  },
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
