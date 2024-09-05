24ab92c0a4a393c3d9fa7224dab0577648230d79
it27427 <istiak.tushar.qtec@gmail.com>
2024-09-05 16:19:50 +0600

    noticetable done and jquery removed

---
+++ assets/js/datatable/noticetable.js
@@ -0,0 +1,11 @@
+new DataTable('#noticetable', {
+  dom: 'lrtip',
+  bLengthChange: false,
+  bInfo: false,
+  language: {
+    paginate: {
+      previous: '<span class="ri-arrow-left-s-line"></span>',
+      next: '<span class="ri-arrow-right-s-line"></span>',
+    },
+  },
+});
