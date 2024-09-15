39bdf08e454c8a4cfaad594dfb5e52b7a4f4076f
it27427 <istiak.tushar.qtec@gmail.com>
2024-09-15 13:07:35 +0600

    text-typed effect added in home-slider

---
+++ assets/js/typed/typing.js
@@ -0,0 +1,24 @@
+const typed1 = new Typed('#element-1', {
+  strings: ['Bangladesh-India Friendship Power Company (Pvt.) Ltd'],
+  typeSpeed: 100,
+  loop: true,
+  showCursor: false,
+});
+const typed2 = new Typed('#element-2', {
+  strings: ['Maitree Super Thermal Power Project'],
+  typeSpeed: 100,
+  loop: true,
+  showCursor: false,
+});
+const typed3 = new Typed('#element-3', {
+  strings: ['2X660 MegaWatts'],
+  typeSpeed: 100,
+  loop: true,
+  showCursor: false,
+});
+const typed4 = new Typed('#element-4', {
+  strings: ['Rampal 1320 MW Coal Power Plant'],
+  typeSpeed: 100,
+  loop: true,
+  showCursor: false,
+});
